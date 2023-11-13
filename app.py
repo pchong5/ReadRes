411a835b3e3f8c9381b89797be985104067ee6cd
pchong5 <48228000+pchong5@users.noreply.github.com>
2023-11-12 18:17:56 -0700

    change mind, going to use pdf miner instead

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, jsonify
-from flask_restful import Api, Resource, reqparse, abort
-from flask_cors import CORS
-
-import os
-import json
+import re
+import time
+from io import StringIO
+import requests
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+from pdf2image import convert_from_path
+import pytesseract
+from pytesseract import image_to_string
+import os.path
+import phonenumbers
+from phonenumbers import timezone
+from datetime import datetime
+import spacy
+from spacy.matcher import PhraseMatcher
+from skillNer.general_params import SKILL_DB
+from skillNer.skill_extractor_class import SkillExtractor
+import pypostalwin
 
